a1d946bebd9ba9b09fc356b050c56a03a23bdea3
seom98 <seo_m98@naver.com>
2025-06-28 17:20:17 +0900

    ✨ feat : 메인화면 변경

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,55 +2,31 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        완전 메롱!
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                {/* <div className={styles.don_2}>
+                    <div className={styles.don1}></div>
+                    <div className={styles.don2}></div>
+                    <div className={styles.don3}></div>
+                    <div className={styles.don4}></div>
+                    <div className={styles.don5}></div>
+                    <div className={styles.don6}></div>
+                    <div className={styles.don7}></div>
+                </div>
+                s */}
+                <div className={styles.dongood}>
+                    <div className={styles.don}>
+                        <div className={styles.don1}></div>
+                        <div className={styles.don2}></div>
+                        <div className={styles.don3}></div>
+                        <div className={styles.don4}></div>
+                        <div className={styles.don5}></div>
+                        <div className={styles.don6}></div>
+                        <div className={styles.don7}></div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }

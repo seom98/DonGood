25c73730704d86fc07bea10f71a3805fe7d2d68b
seom98 <seo_m98@naver.com>
2025-07-28 17:48:05 +0900

    :wrench: refact : 백엔드 제거

--- backend/src/categories/dto/create-category.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateCategoryDto {
-  @ApiProperty({ example: '식비', description: '카테고리 이름' })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-} 
--- backend/src/goals/types/goal-type.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum GoalType {
-  DAILY = 'DAILY',
-  MONTHLY = 'MONTHLY',
-} 

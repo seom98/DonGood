7ec59292937d6edec15d9ddf2af8ca58e18c5008
seom98 <seo_m98@naver.com>
2025-08-29 17:56:49 +0900

    ✨ feat: 메인 페이지 스타일 및 구조 개선
    - globals.css에서 배경색과 텍스트 색상 변경
    - HomePage 컴포넌트 구조를 MainContainer로 변경하여 스타일링 개선
    - 새로운 HomePageStyles.tsx 파일 추가 및 MainContainer 스타일 정의
    - HomePage.module.css에서 불필요한 스타일 제거

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,16 @@
-import styles from "@/styles/pages/HomePage.module.css";
 import HomeHeader from "@/components/common/HomeHeader";
 import SpinDonGood from "@/components/SpinDonGood";
 import StartButton from "@/components/home/StartButton";
+import { MainContainer } from "@/components/home/HomePageStyles";
+import { Text16 } from "@/components/atoms/Text";
 
 export default function HomePage() {
     return (
-        <div>
-            <main className={styles.main}>
-                <HomeHeader />
-                <SpinDonGood />
-                <div>돈 굳었다! 돈, 굳!</div>
-                <StartButton />
-            </main>
-        </div>
+        <MainContainer>
+            <HomeHeader />
+            <SpinDonGood />
+            <Text16>돈 굳었다! 돈, 굳!</Text16>
+            <StartButton />
+        </MainContainer>
     );
 }
---
+++ frontend/src/components/home/HomePageStyles.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+export const MainContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100dvh;
+    padding: 20px;
+    text-align: center;
+    background-color: var(--grey025);
+`;
